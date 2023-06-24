4639666000bb22a523855fab3d35eff5c4263ce2
Manish V <98653583+VDMANISH@users.noreply.github.com>
2023-06-24 15:04:53 +0530

    Add files via upload
    Step 1: Set up an OpenWeatherMap API Key
       Go to the OpenWeatherMap website at https://openweathermap.org/ and sign up for a free account.
       Once you have an account, navigate to the API Keys section and generate a new API key. Copy the API key as we'll need it later.
    Step 2: Install Required Libraries
       Make sure you have Python installed on your system. You can check this by running python --version in your terminal or command prompt.
       Install the requests library by running pip install requests in your terminal or command prompt. This library will help us make HTTP requests to the OpenWeatherMap API.
    Step 3: Set Up the Code
      Open your favorite text editor or integrated development environment (IDE) and create a new Python file (___.py)
    Step 4: Insert the API Key (your API key)
       In the main function, replace  with the API key you obtained from OpenWeatherMap.
    Step 5: Run the Code
       Open a terminal or command prompt and navigate to the directory where the weather.py file is located.
       Run the command python ____.py to execute the script.
       Enter the name of the city for which you want to fetch the weather forecast when prompted.
       The program will then make a request to the OpenWeatherMap API, retrieve the weather data, and display the current temperature and description for the specified city.

---
+++ weforecast.py
@@ -0,0 +1,30 @@
+import requests
+import json
+#1
+def get_weather(api_key, city_name):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
+#2
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        weather_data = response.json()
+        # Extract relevant weather information
+        temperature = weather_data["main"]["temp"]
+        description = weather_data["weather"][0]["description"]
+        print(f"Weather in {city_name}:")
+        print("Temperature:", temperature)
+        print("Description:", description)
+    except requests.exceptions.HTTPError as err:
+        print("Error:", err)
+    except json.JSONDecodeError:
+        print("Error: Failed to parse weather data.")
+    except KeyError:
+        print("Error: Invalid weather data format.")
+#3
+def main():
+    api_key = "4b463278862eba6e4cbf1df5e2a82d73"  # Replace with your OpenWeatherMap API key
+    city_name = input("Enter city name: ")
+    get_weather(api_key, city_name)
+#4
+if __name__ == "__main__":
+    main()           #vdmanish
